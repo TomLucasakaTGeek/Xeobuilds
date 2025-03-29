15246f738b160be9351cd7aa2e3f210980846cee
TomLucasakaTGeek <TanmayEmpowers@proton.me>
2025-03-29 19:01:00 +0530

    Completing db.js file creation for MySQL

--- filesys.js
+++ filesys.js
@@ -13,13 +13,64 @@ const makeDir = fs.mkdir( projPath, { recursive : true }, ( err ) => {
 })
 
 //create files 
+
 // package.json(npm init -y) , 
-fs.appendFile('package.json' , ' { ' )
+fs.appendFile(projPath + '/package.json' , ' { ' )
+
+
+// Models/db.js ,
+const dbjs = ''
+if ( dbchoice === 'MySQL' ) {
+    dbjs = `import mysql from 'mysql2'
+    import dotenv from 'dotenv'
+    dotenv.config()
+    
+    const pool = mysql.createPool({
+        host: process.env.MYSQL_HOST ,
+        user: process.env.MYSQL_USER ,
+        password: process.env.MYSQL_PASSWORD ,
+        database: process.env.MYSQL_DATABASE
+        }).promise()
+        
+        export async function getData() {
+            const [rows] = await pool.query("SELECT * FROM Db")
+            return rows
+            }
+            
+            export async function getDataById(id) {
+                const [rows] = await pool.query(\`
+                SELECT * FROM Db
+                WHERE id = \${id};
+                \`)
+                return rows[0]
+                }
+                
+                export async function createData(title, contents) {
+                    const [result] = await pool.query(\`
+                    INSERT INTO Db (title, contents)
+                    VALUES(?, ?);
+                    \`, [title, contents])
+                    const id = result.insertId 
+                    return getData(id)
+                    }
+                    
+                    const result = await createData('test', 'test')
+                    console.log(result)
+                    
+                    `
+    } else if ( dbchoice === 'MongoDB' ) {
+        dbjs = ``       
+        
+        } else {
+                console.log("error Occured")
+            }
+                            
+    fs.appendFile(projPath + '/Models/db.js', )
+
+
 // Controller/index.js ,
 
 // Middleware/handler.js ,
-
-// Models/db.js ,
 
 //  schema.js(with Zod) ,
 
